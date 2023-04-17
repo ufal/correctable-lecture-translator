fb8445006b1b3ba719b4688a8e2d500905ce44d6
lexi-k <markocechovic@gmail.com>
2023-04-17 07:25:09 +0000

    feat(frontend): add https support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,42 @@
 // Plugins
-import vue from '@vitejs/plugin-vue'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import vue from '@vitejs/plugin-vue';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+
+import fs from 'fs';
+
+let serverKey: Buffer;
+let serverCert: Buffer;
+let serverConfig;
+
+try {
+  serverKey = fs.readFileSync(process.env.SERVERKEY!);
+  serverCert = fs.readFileSync(process.env.SERVERCERT!);
+
+  serverConfig = {
+    port: 5001,
+    https: {
+      key: serverKey,
+      cert: serverCert
+    },
+  }
+}
+catch (e) {
+  console.error(e);
+  console.info("Running http server without SSL.")
+
+  serverConfig = {
+    port: 5001,
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ 
+    vue({
       template: { transformAssetUrls }
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
@@ -35,7 +62,5 @@ export default defineConfig({
       '.vue',
     ],
   },
-  server: {
-    port: 3000,
-  },
+  server: serverConfig,
 })
