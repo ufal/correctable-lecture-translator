e7a4a3b4dc1ac411482712dce7df97d7fe6fb60c
Correctable Lecture Translator <crt@ufal.mff.cuni.cz>
2024-02-04 09:38:29 +0000

    Update GitHub actions

--- backend/run_commands.sh
+++ backend/run_commands.sh
@@ -1,5 +1,5 @@
-~/correctable-lecture-translator/backend/api$ sudo SERVERKEY=serverkey.pem SERVERCERT=servercert.pem /home/speechtransedit/.venv/bin/python3 app.py
-~/correctable-lecture-translator/backend/model-src/model$ /home/speechtransedit/.venv/bin/python3 computation_node_fast.py
-~/correctable-lecture-translator/backend/model-src/model$ /home/speechtransedit/.venv/bin/python3 computation_node_translate.py
+./api$ sudo SERVERKEY=serverkey.pem SERVERCERT=servercert.pem /home/speechtransedit/.venv/bin/python3 app.py
+./model-src/model$ ~/.venv/bin/python3 computation_node_fast.py
+./model-src/model$ ~/.venv/bin/python3 computation_node_translate.py
 
-Gold Drive made by Vorel Vaping Film´s.
+# Gold Drive made by Vorel Vaping Film´s.
